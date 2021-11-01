44a3f73ed5f435ff3f61e25c5e8f3d8166c85440
Abenezer <abentad4@gmail.com>
2021-11-01 16:30:31 +0300

    working state

--- index.js
+++ index.js
@@ -23,5 +23,7 @@ app.use('/uploads/products', express.static('uploads/products'));
 
 
 //connecting to mongodb and starting serve
-const port = process.env.PORT || 3000;
-app.listen(port, ()=> console.log(`Server started at http://localhost:${port}`));
+// const port = process.env.PORT || 3000;
+// app.listen(port, ()=> console.log(`Server started at http://localhost:${port}`));
+app.listen();
+
--- utils/database.js
+++ utils/database.js
@@ -1,18 +1,28 @@
-const mysql = require('mysql');
+const {createPool} = require('mysql');
 
 //mysql connection
-const mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'shopri',
-    multipleStatements: true
+// const mysqlConnection = mysql.createConnection({
+//     port: 3306,
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'shopri',
+//     multipleStatements: true
+// });
+
+const mysqlConnection = createPool({
+    port: 3306,
+    host: "localhost",
+    user: "rentochcom_david",
+    password: "y]~eHw@E*b$%",
+    database: "rentochcom_shopri",
+    connectTimeout: 10
 });
 
-mysqlConnection.connect((error)=>{
-    if(error) console.log(error);
-    else console.log('DB connected!');
-});
+// mysqlConnection.connect((error)=>{
+//     if(error) console.log(error);
+//     else console.log('DB connected!');
+// });
 
 
 module.exports = mysqlConnection;
