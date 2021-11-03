87a67d89fc63aeb7f83d2d00c1b4ec988bde9da4
Abenezer <abentad4@gmail.com>
2021-11-03 16:07:43 +0300

    added find by category

--- routes/data_routes.js
+++ routes/data_routes.js
@@ -39,7 +39,10 @@ router.post('/post', requireAuth, uploadProductImages, modifyProductImage, async
         mysqlConnection.query("INSERT INTO products(name, isPending, views, price, description, category, image, datePosted, posterId, posterName, posterProfileAvatar, posterPhoneNumber)\
         VALUES ('"+ name +"','"+ isPending +"','"+ views +"','"+price+"','" +description+"','"+category+"','"+req.files.path[0]+"','"+datePosted+"','"+posterId+"','"+posterName+"','"+posterProfileAvatar+"','"+posterPhoneNumber+"')"
         ,(error, rows, fields)=>{
-            if(error) console.log(error);
+            if(error) {
+                res.status(500).json({message: "something went wrong"});
+                console.log(error);
+            }
             else{
                 insertedProductId = rows.insertId;
                 let images = [];
@@ -48,11 +51,15 @@ router.post('/post', requireAuth, uploadProductImages, modifyProductImage, async
                         images.push(req.files.path[u]);
                     }
                 } catch (error) {
+                    res.status(500).json({message: "something went wrong"});
                     console.log(`cannot parse because: ${error.message}`);
                 }
                 console.log(`found ${images.length} images`);
                 mysqlConnection.query("INSERT INTO images (id, url) VALUES ?", [images.map(image => [insertedProductId, image])] , (err, rows, fields) => {
-                    if (err) throw err;
+                    if (err) {
+                        res.status(400).json({error});
+                        console.log(err);
+                    }
                     else{
                         insertedProductImageId = rows.insertId;
                         res.status(201).json({insertedProductId});
@@ -61,8 +68,8 @@ router.post('/post', requireAuth, uploadProductImages, modifyProductImage, async
             }
         });
     } catch (error) {
+        res.status(500).json({message: "something went wrong"});
         console.log(error);
-        res.status(400).json({error});
     }
 });
 
@@ -72,24 +79,74 @@ router.get('/products', requireAuth, async (req,res)=> {
     const pendingString = 'false';
     const { size } = req.query;
     const page = req.query.page ? Number(req.query.page) : 1;
-    mysqlConnection.query("SELECT * FROM products WHERE isPending = ?", [pendingString] ,(error, rows, fields)=>{
-        const resLength = rows.length;
-        console.log(`products found: ${resLength}`);
-        const totalPages = Math.ceil(resLength / size);        
-        if(error) console.log(error);
-        if(page > totalPages) res.redirect('/products?page='+ encodeURIComponent(totalPages) + '&size='+ encodeURIComponent(size));
-        else if(page < 1)  res.redirect('/products?page='+ encodeURIComponent('1') + '&size='+ encodeURIComponent(size));
-        const startPoint = (page - 1) * size;
-        mysqlConnection.query(`SELECT * FROM products WHERE isPending = ? ORDER BY id DESC LIMIT ${startPoint},${size}`, [pendingString] ,(error, rows, fields)=>{
-            if(error) console.log(error);
-            let iterator = (page - 5) < 1 ? 1 : page - 5;
-            let endPoint = (iterator + 9) <= totalPages ? (iterator + 9) : page + (totalPages - page);
-            if(endPoint < (page + 4)){
-                iterator -= (page + 4) - totalPages;
+    try {
+        mysqlConnection.query("SELECT * FROM products WHERE isPending = ?", [pendingString] ,(error, rows, fields)=>{
+            const resLength = rows.length;
+            console.log(`products found: ${resLength}`);
+            const totalPages = Math.ceil(resLength / size);        
+            if(error) {
+                res.status(500).json({message: "something went wrong"});
+                console.log(error);
             }
-            res.json({rows, totalPages});
+            if(page > totalPages) res.redirect('/products?page='+ encodeURIComponent(totalPages) + '&size='+ encodeURIComponent(size));
+            else if(page < 1)  res.redirect('/products?page='+ encodeURIComponent('1') + '&size='+ encodeURIComponent(size));
+            const startPoint = (page - 1) * size;
+            mysqlConnection.query(`SELECT * FROM products WHERE isPending = ? ORDER BY id DESC LIMIT ${startPoint},${size}`, [pendingString] ,(error, rows, fields)=>{
+                if(error) {
+                    res.status(500).json({message: "something went wrong"});
+                    console.log(error);
+                }
+                let iterator = (page - 5) < 1 ? 1 : page - 5;
+                let endPoint = (iterator + 9) <= totalPages ? (iterator + 9) : page + (totalPages - page);
+                if(endPoint < (page + 4)){
+                    iterator -= (page + 4) - totalPages;
+                }
+                res.status(200).json({rows, totalPages});
+            });
         });
-    });
+    } catch (error) {
+        res.status(500).json({message: "something went wrong"});
+        console.log(error);
+    }
+
+});
+
+//for getting all products based on product category
+router.get('/products/category', requireAuth, async (req,res)=> {
+    console.log('get products called');
+    const pendingString = 'false';
+    const { size, category } = req.query;
+    const page = req.query.page ? Number(req.query.page) : 1;
+    try {
+        mysqlConnection.query("SELECT * FROM products WHERE isPending = ? AND category = ?", [pendingString, category] ,(error, rows, fields)=>{
+            const resLength = rows.length;
+            console.log(`products found: ${resLength}`);
+            const totalPages = Math.ceil(resLength / size);        
+            if(error) {
+                res.status(500).json({message: "something went wrong"});
+                console.log(error);
+            }
+            if(page > totalPages) res.redirect('/products?page='+ encodeURIComponent(totalPages) + '&size='+ encodeURIComponent(size));
+            else if(page < 1)  res.redirect('/products?page='+ encodeURIComponent('1') + '&size='+ encodeURIComponent(size));
+            const startPoint = (page - 1) * size;
+            mysqlConnection.query(`SELECT * FROM products WHERE isPending = ? AND category = ? ORDER BY id DESC LIMIT ${startPoint},${size}`, [pendingString, category] ,(error, rows, fields)=>{
+                if(error) {
+                    res.status(500).json({message: "something went wrong"});
+                    console.log(error);
+                }
+                let iterator = (page - 5) < 1 ? 1 : page - 5;
+                let endPoint = (iterator + 9) <= totalPages ? (iterator + 9) : page + (totalPages - page);
+                if(endPoint < (page + 4)){
+                    iterator -= (page + 4) - totalPages;
+                }
+                res.status(200).json({rows, totalPages});
+            });
+        });
+    } catch (error) {
+        res.status(500).json({message: "something went wrong"});
+        console.log(error);
+    }
+    
 });
 
 //for getting all products
@@ -98,43 +155,72 @@ router.get('/products/pending', requireAuth, async (req,res)=> {
     const pendingString = 'true';
     const { size } = req.query;
     const page = req.query.page ? Number(req.query.page) : 1;
-    mysqlConnection.query("SELECT * FROM products WHERE isPending = ?", [pendingString] ,(error, rows, fields)=>{
-        const resLength = rows.length;
-        console.log(`products found: ${resLength}`);
-        const totalPages = Math.ceil(resLength / size);        
-        if(error) console.log(error);
-        if(page > totalPages) res.redirect('/products?page='+ encodeURIComponent(totalPages) + '&size='+ encodeURIComponent(size));
-        else if(page < 1)  res.redirect('/products?page='+ encodeURIComponent('1') + '&size='+ encodeURIComponent(size));
-        const startPoint = (page - 1) * size;
-        mysqlConnection.query(`SELECT * FROM products WHERE isPending = ? ORDER BY id DESC LIMIT ${startPoint},${size}`, [pendingString] ,(error, rows, fields)=>{
-            if(error) console.log(error);
-            let iterator = (page - 5) < 1 ? 1 : page - 5;
-            let endPoint = (iterator + 9) <= totalPages ? (iterator + 9) : page + (totalPages - page);
-            if(endPoint < (page + 4)){
-                iterator -= (page + 4) - totalPages;
+    try {
+        mysqlConnection.query("SELECT * FROM products WHERE isPending = ?", [pendingString] ,(error, rows, fields)=>{
+            const resLength = rows.length;
+            console.log(`products found: ${resLength}`);
+            const totalPages = Math.ceil(resLength / size);        
+            if(error) {
+                res.status(500).json({message: "something went wrong"});
+                console.log(error);
             }
-            res.json({rows, totalPages});
+            if(page > totalPages) res.redirect('/products?page='+ encodeURIComponent(totalPages) + '&size='+ encodeURIComponent(size));
+            else if(page < 1)  res.redirect('/products?page='+ encodeURIComponent('1') + '&size='+ encodeURIComponent(size));
+            const startPoint = (page - 1) * size;
+            mysqlConnection.query(`SELECT * FROM products WHERE isPending = ? ORDER BY id DESC LIMIT ${startPoint},${size}`, [pendingString] ,(error, rows, fields)=>{
+                if(error) {
+                    res.status(500).json({message: "something went wrong"});
+                    console.log(error);
+                }
+                let iterator = (page - 5) < 1 ? 1 : page - 5;
+                let endPoint = (iterator + 9) <= totalPages ? (iterator + 9) : page + (totalPages - page);
+                if(endPoint < (page + 4)){
+                    iterator -= (page + 4) - totalPages;
+                }
+                res.json({rows, totalPages});
+            });
         });
-    });
+    } catch (error) {
+        res.status(500).json({message: "something went wrong"});
+        console.log(error);
+    }
 });
 
 //get specific product by its id
 router.get('/product',(req,res)=>{
     const { id } = req.query;
-    mysqlConnection.query('SELECT * FROM products WHERE id = ?', [id], (error, rows, fields)=>{
-        if(error) console.log(error);
-        else res.json(rows[0]);
-    });
+    try {
+        mysqlConnection.query('SELECT * FROM products WHERE id = ?', [id], (error, rows, fields)=>{
+            if(error) {
+                res.status(500).json({message: "something went wrong"});
+                console.log(error);
+            }
+            else res.json(rows[0]);
+        });
+    } catch (error) {
+        res.status(500).json({message: "something went wrong"});
+        console.log(error);
+    }
+
 });
 
 //update product by its id
 router.patch('/product',(req,res)=>{
     const { id } = req.query;
     const isPendingString = 'false';
-    mysqlConnection.query('UPDATE products SET isPending = ? WHERE id = ?', [ isPendingString, id ], (error, rows, fields)=>{
-        if(error) console.log(error);
-        else res.status(200).json({message: "updated successfully"});
-    });
+    try {
+        mysqlConnection.query('UPDATE products SET isPending = ? WHERE id = ?', [ isPendingString, id ], (error, rows, fields)=>{
+            if(error) {
+                res.status(500).json({message: "something went wrong"});
+                console.log(error);
+            }
+            else res.status(200).json({message: "updated successfully"});
+        });
+    } catch (error) {
+        res.status(500).json({message: "something went wrong"});
+        console.log(error);
+    }
+    
 });
 
 
@@ -143,7 +229,10 @@ router.patch('/product',(req,res)=>{
 router.delete('/product',(req,res)=>{
     const { id } = req.query;
     mysqlConnection.query('SELECT * FROM images WHERE id = ?', [id], (error, rows, fields)=>{
-        if(error) console.log(error);
+        if(error) {
+            res.status(500).json({message: "something went wrong"});
+            console.log(error);
+        }
         else {
             for(let v = 0; v < rows.length; v++){
                 try {
@@ -153,14 +242,17 @@ router.delete('/product',(req,res)=>{
                     console.log(error.message);
                 }
             }
-        }
-    });
-    mysqlConnection.query('DELETE FROM images WHERE id = ?', [id], (error, rows, fields)=>{
-        if(error) console.log(error);
-        else {
-            mysqlConnection.query('DELETE FROM products WHERE id = ?', [id], (error, rows, fields)=>{
+            mysqlConnection.query('DELETE FROM images WHERE id = ?', [id], (error, rows, fields)=>{
                 if(error) console.log(error);
-                else res.status(200).json({message: 'Deleted Successfully'});
+                else {
+                    mysqlConnection.query('DELETE FROM products WHERE id = ?', [id], (error, rows, fields)=>{
+                        if(error) {
+                            res.status(500).json({message: "something went wrong"});
+                            console.log(error);
+                        }
+                        else res.status(200).json({message: 'Deleted Successfully'});
+                    });
+                }
             });
         }
     });
